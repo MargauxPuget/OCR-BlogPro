414de43c39d9588ffbe8731f48b12c54a359d94a
MargauxPuget <margaux.puget@gmail.com>
2024-02-16 15:59:28 +0100

    WIP page home

---
+++ app/twig/Twig.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace MPuget\blog\twig;
+
+class Twig
+{
+    private $twig;
+    private $loader;
+
+    public function __construct()
+    {
+        $this->loader = new \Twig\Loader\FilesystemLoader('templates');
+        $this->twig = new \Twig\Environment($this->loader, [
+            'debug' => true,
+        ]);
+        //$this->twig->addGlobal('session', unserialize($_SESSION['user']));
+
+        $this->twig->addExtension(new \Twig\Extension\DebugExtension());
+        //$this->twig->addExtension(new \App\Libs\twigFiltersExtensions());
+    }
+    public function getTwig()
+    {
+        return $this->twig;
+    }
+}
--- index.php
+++ index.php
@@ -1,3 +1,68 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
+
+// require de nos Controllers
+use MPuget\blog\Controllers\CoreController;
+use MPuget\blog\Controllers\MainController;
+use MPuget\blog\Controllers\ErrorController;
+
+//* -----------------------------------------------------
+//*                Routeur : AltoRouter
+//* -----------------------------------------------------
+
+// On commence par instancier un objet AltoRouter
+$router = new AltoRouter();
+// echo get_class($router);
+// on donne à AltoRouter la partie de l'URL à ne pas prendre en compte pour faire la 
+// comparaison entre l'URL demandée par le visiteur (exemple /categoy/1) et l'URL de notre route
+$publicFolder = dirname($_SERVER['SCRIPT_NAME']);
+$router->setBasePath($publicFolder);
+
+
+// On va ensuite pouvoir mapper nos routes
+$router->map(
+    'GET',
+    '', // l'URL de cette route
+    // target :
+    [
+        'action' => 'home', // méthode à appeler
+        'controller' => 'MPuget\blog\Controllers\MainController' // controller concerné
+    ],
+    'home' // le nom qu'on donne à notre route (pour $router->generate())
+);
+$router->generate('home');
+
+
+
+$match = $router->match();
+
+//* -----------------------------------------------------
+//*                     Dispatcher
+//* -----------------------------------------------------
+
+// est-ce que notre route existe ? 
+if($match) {
+  // notre route existe, on va récupérer les données de la route 
+  // que l'on a définit précédemment avec $router->map()
+
+  // on récupère le controller
+  $controllerName= $match['target']['controller'];
+
+  //$match['target'] = 3ème paramètre défini dans les méthodes $router->map() ci-dessus
+
+  // on récupère la méthode
+  $method = $match['target']['action'];
+
+  // on peut instancier notre controller
+  $controller = new $controllerName();
+
+  // on peut appeler la méthode de notre controller
+  // on va envoyer les paramètres éventuels à notre méthode
+  // ces paramètres étant ceux définis avec $router->map() ci dessus ! [i:id]
+  $controller->$method($match['params']);
+} else {
+  // notre route n'existe pas, donc on renvoit sur une 404 !
+  $controller = new ErrorController();
+  $controller->error404();
+}
