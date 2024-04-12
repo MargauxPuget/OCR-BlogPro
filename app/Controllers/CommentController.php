3832dfbc0b6b5136a1020001c9283419c513e574
MargauxPuget <margaux.puget@gmail.com>
2024-04-12 09:40:53 +0200

    early return

--- app/Controllers/CommentController.php
+++ app/Controllers/CommentController.php
@@ -24,19 +24,24 @@ class CommentController
         // vérifier que l'utilisateur est un admin
         $user = $this->userRepo->find($_SESSION['userId']);
 
-        $dataNewComment = $_POST;
-        if ($user->getRole() === 1) {
-            // modifier le status du commentaire
-            $comment = $this->commentRepo->find($params['commentId']);
-            
-            if (isset($dataNewComment['action'])
-                && (($dataNewComment['action']) === "validated"
-                || ($dataNewComment['action']) === "pause"
-                || ($dataNewComment['action']) === "refused")
-            ) {
-                $comment = $this->commentRepo->changedStatusComment($comment, $dataNewComment['action']);
-            }
+        if ($user->getRole() != 1) {
+            header('Location: /user/' . $user->getId());
+            return;
         }
+
+        $dataNewComment = $_POST;
+
+        // modifier le status du commentaire
+        $comment = $this->commentRepo->find($params['commentId']);
+        
+        if (isset($dataNewComment['action'])
+            && (($dataNewComment['action']) === "validated"
+            || ($dataNewComment['action']) === "pause"
+            || ($dataNewComment['action']) === "refused")
+        ) {
+            $comment = $this->commentRepo->changedStatusComment($comment, $dataNewComment['action']);
+        }
+        
         header('Location: /user/' . $user->getId());
     }
 }
