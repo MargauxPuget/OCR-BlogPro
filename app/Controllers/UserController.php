cebe54a2221b0405295cd985515019a3764e96eb
MargauxPuget <margaux.puget@gmail.com>
2024-05-24 11:13:26 +0200

    after PR

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -215,7 +215,7 @@ class UserController
         $sessionUser = $this->userRepo->getSessionUser();
         if (
             $params['userId'] != $sessionUser->getId()
-            || $sessionUser->getRole() != 1
+            || !($sessionUser->getRole() == 1)
         ){
             header('Location: /');
             return;
