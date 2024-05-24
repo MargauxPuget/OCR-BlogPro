2346e777098b60e945f4ebc069fa6805458cacc5
MargauxPuget <margaux.puget@gmail.com>
2024-05-24 10:56:30 +0200

    creation to function getSessionUser()

--- app/Controllers/CommentController.php
+++ app/Controllers/CommentController.php
@@ -43,10 +43,10 @@ class CommentController
 
     public function updateComment($params) {
         // vérifier que l'utilisateur est un admin
-        $user = $this->userRepo->find($_SESSION['userId']);
+        $sessionUser = $this->userRepo->getSessionUser();
 
-        if ($user->getRole() != 1) {
-            header('Location: /user/' . $user->getId());
+        if ($sessionUser->getRole() != 1) {
+            header('Location: /user/' . $sessionUser->getId());
             return;
         }
 
--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -80,12 +80,12 @@ class UserController
         }
 
         // récupération de cet utilisateur
-        $user = $this->userRepo->find($_SESSION['userId']);
+        $sessionUser = $this->userRepo->getSessionUser();
 
         // récupération des commentaires de cet utilisateur
-        $commentsByUser = $this->commentRepo->findAllforOneUser($user);
+        $commentsByUser = $this->commentRepo->findAllforOneUser($sessionUser);
         // récupération des commentaires de cet utilisateur
-        $commentsForValidation = $this->commentRepo->findAllforOneUser($user, 0);
+        $commentsForValidation = $this->commentRepo->findAllforOneUser($sessionUser, 0);
         // récupération des commentaires de cet utilisateur
         $allPosts = $this->postRepo->findFromStatus('active', 3);
 
